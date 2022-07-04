c0da64fa8df9893cd38dd90eefef8975c92820c5
Marius <marius@example.com>
2022-07-04 15:02:30 +0300

    uzd1

---
+++ readme.md
@@ -0,0 +1,14 @@
+# js intro
+
+## uzd
+
+1. sukurti kintamuosius 'vardas', 'amzius', 'arVairuojuMasina', 'miestas', 'megstamiausiaSalis'
+
+2. surasyti i juos reiksmes
+3. atspausdinti reiksmes konsoleje
+
+4. sukurti 4 skaicius kintamuosius
+5. apskaiciuoti ju vidurki ir priskirti kintamajam avg
+
+6. sukurti kintamaji 'pavarde' ir priskirti jam reiksme
+7. atspausdinti su konsole varda is pirmo pratimo ir pavarde is 6 pratimo. pvz 'James Bond'
--- uzd1.js
+++ uzd1.js
@@ -1,5 +1,18 @@
 'use strict';
 // sukurti kintamuosius 'vardas', 'amzius', 'arVairuojuMasina', 'miestas', 'megstamiausiaSalis'
+let vardas;
+let amzius;
+let arVairuojuMasina;
+let miestas;
+let megstamiausiaSalis;
 // surasyti i juos reiksmes
+vardas = 'James';
+amzius = 33;
+arVairuojuMasina = true;
+miestas = 'London';
+megstamiausiaSalis = 'Bahamas';
 
 // atspausdinti reiksmes konsoleje
+
+// sukurti 4 skaicius kintamuosius
+// apskaiciuoti ju vidurki ir priskirti kintamajam avg
